dcf4942d68c069683ecfc73caacd3d6f59788b0a
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-04-05 01:35:21 +0530

    user_models added

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -1,11 +1,56 @@
-export const signup = (req,res)=>{
-    console.log("signUser");
-}
+import User from "../models/user.models.js";
+
+export const signup = async (req,res)=>{
+    try {
+        const {fullname,username,password,confirmPassword,gender} = req.body;
+
+        if(password !== confirmPassword){
+            return res.status(400).json({erroe:"password don't match"});
+        }
+
+        const user = await User.findOne({username});
+
+        if(user){
+            return res.status(400).json({error:"Username already exits"});
+        }
+
+
+        const boyProfilePic = `https://avatar.iran.liara.run/public/boy?username=${username}`;
+		const girlProfilePic = `https://avatar.iran.liara.run/public/girl?username=${username}`;
+
+
+        const newUser = new User({
+            fullname,
+            username,
+            password,
+            gender,
+            profilePic: gender === "male" ? boyProfilePic : girlProfilePic,
+        });
+
+        await newUser.save();
+
+        res.status(201).json({
+            _id:newUser._id,
+            fullname:newUser.fullname,
+            username: newUser.username,
+            profilePic:newUser.profilePic,
+        });
+
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+    }
+};
+
+
+
+
+
 
 export const login = (req,res)=>{
     console.log("loginUser");
-}
+};
 
 export const logout = (req,res)=>{
     console.log("logoutUser");
-}
+};
--- backend/server.js
+++ backend/server.js
@@ -1,18 +1,26 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import connectToMongoDB from "../mongo_db/connect_mongo_db.js";
 
 
 const app = express();
-dotenv.config();
 const PORT = process.env.PORT || 5000;
 
 
-app.get("/",(req,res)=>{
+dotenv.config();
 
-    res.send("Hello World!!!");
+
+app.use(express.json())
+app.use("/api/auth",authRoutes);
+
+
+// app.get("/",(req,res)=>{
+//     res.send("Hello World!!!");
+// });
+
+
+app.listen(PORT,() =>{
+    connectToMongoDB();
+    console.log(`Server running on port ${PORT}`);
 });
-
-app.use("/api/auth",authRoutes)
-
-app.listen(5000,() => console.log(`Server running on port ${PORT}`) );
