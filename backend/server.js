e033bc4ea82d54ae3c840e85df675a4187bb944a
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-04-05 23:22:31 +0530

    messageRoutes and message_controller

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import authRoutes from "./routes/auth.routes.js";
+import messageRoutes from "./routes/message.routes.js";
 import connectToMongoDB from "../mongo_db/connect_mongo_db.js";
 
 
@@ -11,8 +12,11 @@ const PORT = process.env.PORT || 5000;
 dotenv.config();
 
 
-app.use(express.json())
+app.use(express.json());
+
 app.use("/api/auth",authRoutes);
+app.use("/api/message",messageRoutes);
+
 
 
 // app.get("/",(req,res)=>{
