c4d2ff8e3f262f922bc65a809291ebcbe59accb0
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-04-05 22:40:48 +0530

    login,logout

--- backend/controllers/auth.controller.js
+++ backend/controllers/auth.controller.js
@@ -54,14 +54,37 @@ export const signup = async (req, res) => {
 };
 
 
+export const login = async (req,res)=>{
+    try {
+        const { username, password } = req.body;
+		const user = await User.findOne({ username });
+		const isPasswordCorrect = await bcrypt.compare(password, user?.password || "");
 
+		if (!user || !isPasswordCorrect) {
+			return res.status(400).json({ error: "Invalid username or password" });
+		}
 
+		generateTokenAndSetCookie(user._id, res);
 
-
-export const login = (req,res)=>{
-    console.log("loginUser");
+		res.status(200).json({
+			_id: user._id,
+			fullName: user.fullName,
+			username: user.username,
+			profilePic: user.profilePic,
+		});
+        
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+    }
 };
 
-export const logout = (req,res)=>{
-    console.log("logoutUser");
+export const logout =  async (req,res)=>{
+    try {
+        res.cookie("jwt", "", { maxAge: 0 });
+		res.status(200).json({ message: "Logged out successfully" });
+    } catch (error) {
+        console.log("Error in signup controller", error.message);
+		res.status(500).json({ error: "Internal Server Error" });
+    }
 };
