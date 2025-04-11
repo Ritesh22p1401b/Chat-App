04683efb8118f479e465e473970cd1bac136d784
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-04-11 15:41:28 +0530

    added_tailwindcss

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -8,26 +8,14 @@ function App() {
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <button className="btn btn-outline">Default</button>
+      <button className="btn btn-outline btn-primary">Primary</button>
+      <button className="btn btn-outline btn-secondary">Secondary</button>
+      <button className="btn btn-outline btn-accent">Accent</button>
+      <button className="btn btn-outline btn-info">Info</button>
+      <button className="btn btn-outline btn-success">Success</button>
+      <button className="btn btn-outline btn-warning">Warning</button>
+      <button className="btn btn-outline btn-error">Error</button>
     </>
   )
 }
