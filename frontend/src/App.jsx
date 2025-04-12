4bf70bb3a29b087fb35da3ccc5f5ec401446f6b0
Ritesh Pandey <ritesh2897pandey@gmail.com>
2025-04-12 15:01:12 +0530

    pages and components

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,23 +1,24 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { Navigate, Route, Routes } from "react-router-dom";
+import "./App.css";
+import Home from "./pages/home/Home";
+import Login from "./pages/login/Login";
+import SignUp from "./pages/signup/SignUp";
+// import { Toaster } from "react-hot-toast";
+// import { useAuthContext } from "./context/AuthContext";
 
 function App() {
-  const [count, setCount] = useState(0)
+	const { authUser } = useAuthContext();
 
-  return (
-    <>
-      <button className="btn btn-outline">Default</button>
-      <button className="btn btn-outline btn-primary">Primary</button>
-      <button className="btn btn-outline btn-secondary">Secondary</button>
-      <button className="btn btn-outline btn-accent">Accent</button>
-      <button className="btn btn-outline btn-info">Info</button>
-      <button className="btn btn-outline btn-success">Success</button>
-      <button className="btn btn-outline btn-warning">Warning</button>
-      <button className="btn btn-outline btn-error">Error</button>
-    </>
-  )
+	return (
+		<div className='p-4 h-screen flex items-center justify-center'>
+			<Routes>
+				<Route path='/' element={authUser ? <Home /> : <Navigate to={"/login"} />} />
+				<Route path='/login' element={authUser ? <Navigate to='/' /> : <Login />} />
+				<Route path='/signup' element={authUser ? <Navigate to='/' /> : <SignUp />} />
+			</Routes>
+			<Toaster />
+		</div>
+	);
 }
 
-export default App
+export default App;
